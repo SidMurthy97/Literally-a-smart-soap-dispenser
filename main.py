edda41766eaa5d00a2d80ef83ee3d1f931857fa7
SidMurthy97 <murthy.siddhant@googlemail.com>
2020-05-09 20:11:37 +0100

    basic web scraping working

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from gtts import gTTS
 import os 
+from bs4 import BeautifulSoup as soup
+import requests
 
-test_text = 'hello this is a test'
-language = 'en' 
+def text_to_speech():
+    test_text = 'Hi, this is Anakin Skywalker and I want to let you know, Dishita Murthy, that you suck at Zelda'
+    language = 'en' 
 
-tts = gTTS(text=test_text,lang=language)
+    tts = gTTS(text=test_text,lang=language)
 
-tts.save("test.mp3")
-os.system("test.mp3")
+    tts.save("test.mp3")
+    os.system("test.mp3")
+
+
+if __name__ == '__main__':
+    
+    page = requests.get("https://www.thefactsite.com/1000-interesting-facts/")
+    Soup = soup(page.content, 'html.parser')
+    print(len(Soup.find_all('p')))
+    
+    for i in range(len(Soup.find_all('p'))):
+        print(i, Soup.find_all('p')[i].get_text())
+    
+    #print(soup.prettify())
