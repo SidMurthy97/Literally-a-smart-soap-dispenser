b8d6a00d697b0e85cbe280dc949b6cd5c9f5a716
SidMurthy97 <murthy.siddhant@googlemail.com>
2020-05-15 21:59:19 +0100

    changed code structure to increase efficiency by preloading a soundbyte before trigger. Currently there is no sensor feedback but this is to be added. Also need to explore how to better punctuate the text to speech system

--- main.py
+++ main.py
@@ -7,26 +7,31 @@ import RPi.GPIO as GPIO
 import time
 import pygame
 
+#setup object
+
+#GPIO/servo
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+servo = GPIO.PWM(11,50)
+
+#TTS/pygame
+language = 'en'
+pygame.mixer.init()
+
+#web scrape
+page = requests.get("https://www.thefactsite.com/1000-interesting-facts/")
+Soup = soup(page.content, 'html.parser')
+
 def text_to_speech(fact):
     
     fact = "Hello! Did you know? " + fact + "Thanks for listening, have a great day!"
-    
-    language = 'en' 
-
     tts = gTTS(text=fact,lang=language)
-
     tts.save("fact.mp3")
-    #os.system("mpg321 fact.mp3")
-    pygame.mixer.init()
     pygame.mixer.music.load("fact.mp3")
-    pygame.mixer.music.play()
+    
 
 def get_fact():
     fact_length = 0
-    
-    page = requests.get("https://www.thefactsite.com/1000-interesting-facts/")
-    Soup = soup(page.content, 'html.parser')
-
     while fact_length < 150:
         random_index = random.randint(7,116)
         fact = Soup.find_all('p')[random_index].get_text()
@@ -53,13 +58,21 @@ def run_servo(servo):
 
 if __name__ == '__main__':
     
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(11,GPIO.OUT)
-
-    servo = GPIO.PWM(11,50)
-    
-    
     fact = get_fact()
     text_to_speech(fact)
-    run_servo(servo)
+    
+    for i in range(5):
+        print(i)
+        x = input("enter number")
+        
+        if x == '1':
+            pygame.mixer.music.play()
+            run_servo(servo)
+            
+            while pygame.mixer.music.get_busy() == True:
+                continue
+            #load next fact 
+            fact = get_fact()
+            text_to_speech(fact)
+            
 
